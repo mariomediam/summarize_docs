44ca1f0d2e8a0af94607b32b7dbb3b1b154d6c18
Your Name <you@example.com>
2024-09-13 15:18:07 -0500

    feat summarize docs

--- miapp/views.py
+++ miapp/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from rest_framework.views import APIView  
+from rest_framework.generics import CreateAPIView
 from django.http import JsonResponse  
+
+import getpass
+import os
+
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_openai import ChatOpenAI
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.llm import LLMChain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import PyPDFLoader
+
+from .serializers import UploadFileSerializer
+
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+PATH_DOCS = os.environ.get('PATH_DOCS')
 
 # Create your views here.
 class HomeView(APIView):  
@@ -9,3 +26,45 @@ class HomeView(APIView):
     return JsonResponse({"message":
     'HOLA MUNDO DESDE DJANGO Y DOCKER', "content":
     'Por Mario Medina'}) 
+ 
+class SumarizeView(CreateAPIView):  
+   serializer_class = UploadFileSerializer
+
+   def post(self, request, format=None):
+
+      try:
+
+         file_name = "doc_request.pdf"
+         location = "{}/".format(os.environ.get("PATH_DOCS"))
+
+         dataArchivo = request.FILES.copy()
+         dataArchivo["location"] = location
+         dataArchivo["file_name"] = file_name
+         data = self.serializer_class(data=dataArchivo)
+
+         if data.is_valid():
+            file_name_save = data.save()  
+            docs = PyPDFLoader(f"{location}{file_name_save}").load()
+
+            os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY   
+
+            llm = ChatOpenAI(model="gpt-4o-mini")
+            prompt = ChatPromptTemplate.from_messages(
+               [("system", "Escribe un resumen conciso de lo siguiente.:\\n\\n{context}")]
+            )
+
+            # Instantiate chain
+            chain = create_stuff_documents_chain(llm, prompt)
+
+            # Invoke chain
+            result = chain.invoke({"context": docs})
+
+            return JsonResponse({"message":
+            '', "content":
+            result})
+         
+      except Exception as e:
+         print(e)
+         return JsonResponse({"message":
+         'Error al procesar la solicitud', "content":
+         ''})
--- miproject/settings.py
+++ miproject/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for miproject project.
 
@@ -11,9 +12,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 
 # Quick-start development settings - unsuitable for production
